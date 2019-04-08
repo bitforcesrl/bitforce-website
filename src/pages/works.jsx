6a6767768f0dd3b5af34c3deb04d4b6241621229
Aleandro <a.coppola@bitforce.app>
2019-04-08 14:31:25 +0200

    add services and works page

---
+++ src/pages/works.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import get from 'lodash/get';
+import Helmet from 'react-helmet';
+
+class HomeIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+    const page = get(this, 'props.data.contentfulPage');
+
+    return (
+      <div style={{ background: '#fff' }}>
+        <Helmet title={siteTitle} />
+        <div>{page.title}</div>
+      </div>
+    );
+  }
+}
+
+export default HomeIndex;
+
+export const pageQuery = graphql`
+  query WorksPageIndex {
+    contentfulPage(slug: { eq: "works" }, node_locale: { eq: "it-IT" }) {
+      title
+      metaDescription {
+        childMarkdownRemark {
+          html
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
